2e9a1106995d3205e7c8b1d6aa3f8869f3e012f1
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-06-10 12:52:19 +0500

    added add job

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,26 @@ import { HomePage } from "./pages/HomePage";
 import { MainLayout } from "./layouts/MainLayout";
 import { JobsPage } from "./pages/JobsPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  )
-);
+import { JobPage, jobLoader } from "./pages/JobPage";
+import { AddJobPage } from "./pages/AddJobPage";
 
 const App = () => {
+  const addJob = (newJob) => {
+    console.log("Adding job", newJob);
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
   return <RouterProvider router={router} />;
-  {
-    /* <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </RouterProvider> */
-  }
 };
 
 export default App;
